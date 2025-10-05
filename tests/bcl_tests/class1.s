f45415bf1c0f7927e7bcac6e0865f0207f1c03fd
alban <alban.read@googlemail.com>
2025-10-05 06:08:24 +0100

    Sun 5th Oct

--- tests/bcl_tests/class1.s
+++ tests/bcl_tests/class1.s
@@ -1,14 +1,13 @@
-lets .section __TEXT,__text,regular,pure_instructions
+.section __TEXT,__text,regular,pure_instructions
 .globl _start
 .globl _START
 .globl _HeapManager_exit_scope
 .globl _WRITEN
+.globl _SDL2_DRAW_RECT
 .globl _WRITES
 .globl _BCPL_LIST_APPEND_STRING
 .globl _WRITEF
 .globl _BCPL_LIST_GET_NTH
-.globl _FILE_OPEN_READ
-.globl _BCPL_CHECK_AND_DISPLAY_ERRORS
 .globl _RDCH
 .globl _BCPL_ALLOC_WORDS
 .globl _FILE_READ
@@ -17,6 +16,7 @@ lets .section __TEXT,__text,regular,pure_instructions
 .globl _SPLIT
 .globl _BCPL_LIST_CREATE_EMPTY
 .globl _GET_FREE_LIST_HEAD_ADDR
+.globl _BCPL_CHECK_AND_DISPLAY_ERRORS
 .globl _UNPACKSTRING
 .globl _SDL2_DELAY
 .globl _FREEVEC
@@ -28,30 +28,30 @@ lets .section __TEXT,__text,regular,pure_instructions
 .globl _NEWLINE
 .globl _BCPL_LIST_GET_HEAD_AS_FLOAT
 .globl _OBJECT_HEAP_FREE
-.globl _FPND
 .globl _FEXP
+.globl _FPND
 .globl _WRITEF4
 .globl _HEAPMANAGER_ENTER_SCOPE
-.globl _FCOS
-.globl _APND
 .globl _WRITEF2
 .globl _BCPL_CLEAR_ERRORS
 .globl _FILTER
 .globl _FLOG
 .globl _GETVEC
+.globl _FGETVEC
 .globl _LPND
 .globl _BCPL_LIST_APPEND_INT
 .globl _RETURNNODETOFREELIST
+.globl _FCOS
+.globl _APND
 .globl _MALLOC
 .globl _JOIN
 .globl _BCPL_FREE_CELLS
+.globl _DEEPCOPYLITERALLIST
 .globl _STRCOPY
+.globl _WRITEF7
+.globl _SDL2_DESTROY_WINDOW
+.globl _SDL2_SET_WINDOW_TITLE
 .globl _PACKSTRING
-.globl _BCPL_FREE_LIST_SAFE
-.globl _DEEPCOPYLIST
-.globl _BCPL_FREE_LIST
-.globl _FILE_CLOSE
-.globl _FIND
 .globl _WRITEF3
 .globl _BCPL_GET_ATOM_TYPE
 .globl _FILE_READS
@@ -64,6 +64,9 @@ lets .section __TEXT,__text,regular,pure_instructions
 .globl _SDL2_CREATE_WINDOW
 .globl _SLURP
 .globl _RAND
+.globl _SDL2_CLEAR
+.globl _SDL2_DESTROY_RENDERER
+.globl _BCPL_LIST_GET_REST
 .globl _WRITEF5
 .globl _BCPL_ALLOC_CHARS
 .globl _OBJECT_HEAP_ALLOC
@@ -77,60 +80,58 @@ lets .section __TEXT,__text,regular,pure_instructions
 .globl _FILE_OPEN_APPEND
 .globl _SDL2_CREATE_WINDOW_EX
 .globl _FSIN
-.globl _SDL2_GET_CURRENT_VIDEO_DRIVER
-.globl _SDL2_INIT_SUBSYSTEMS
-.globl _SDL2_GET_VIDEO_DRIVERS
-.globl _FINISH
-.globl _SDL2_GET_EVENT_MOUSE
-.globl _SDL2_SET_WINDOW_TITLE
-.globl _WRITEF7
-.globl _SDL2_DESTROY_WINDOW
 .globl _HeapManager_enter_scope
 .globl _FABS
-.globl _DEEPCOPYLITERALLIST
+.globl _FILE_OPEN_READ
+.globl _SDL2_GET_CURRENT_VIDEO_DRIVER
+.globl _SDL2_INIT_SUBSYSTEMS
+.globl _BCPL_FREE_LIST_SAFE
+.globl _DEEPCOPYLIST
+.globl _BCPL_FREE_LIST
+.globl _FILE_CLOSE
 .globl _FIX
 .globl _WRITEF1
 .globl _FILE_WRITE
 .globl _COPYLIST
-.globl _SDL2_DRAW_RECT
 .globl _SDL2_GET_DISPLAY_MODES
 .globl _BCPL_GET_LAST_ERROR
 .globl _SDL2_PRESENT
 .globl _SPND
 .globl _SDL2_CREATE_RENDERER_EX
-.globl _BCPL_LIST_GET_REST
-.globl _SDL2_CLEAR
-.globl _SDL2_DESTROY_RENDERER
+.globl _SDL2_GET_EVENT_MOUSE
 .globl _SDL2_SET_DRAW_COLOR
+.globl _WRITEF6
+.globl _HEAPMANAGER_SETSAMMENABLED
+.globl _FILE_OPEN_WRITE
+.globl _SDL2_GET_TICKS
 .globl _SDL2_INIT
 .globl _SDL2_FILL_RECT
 .globl _FWRITE
 .globl _CONCAT
 .globl _SDL2_DRAW_LINE
-.globl _SDL2_CREATE_RENDERER
-.globl _SDL2_CLEAR_ERROR
-.globl _SDL2_SET_WINDOW_SIZE
-.globl _SETTYPE
-.globl _FILE_SEEK
-.globl _SDL2_GET_VERSION
 .globl _SDL2_DRAW_POINT
 .globl _HEAPMANAGER_EXIT_SCOPE
 .globl _SDL2_GET_EVENT_BUTTON
-.globl _WRITEF6
-.globl _HEAPMANAGER_SETSAMMENABLED
-.globl _FILE_OPEN_WRITE
-.globl _SDL2_GET_TICKS
-.globl ___SDL2_STATIC_BUILD
+.globl _SETTYPE
+.globl _FILE_SEEK
+.globl _SDL2_GET_VERSION
+.globl _SDL2_CREATE_RENDERER
+.globl _SDL2_CLEAR_ERROR
+.globl _SDL2_SET_WINDOW_SIZE
 .globl _HEAPMANAGER_WAITFORSAMM
 .globl _RND
 .globl _STRLEN
 .globl _SDL2_POLL_EVENT
+.globl _FINISH
+.globl _SDL2_GET_VIDEO_DRIVERS
+.globl _FIND
 .globl _SDL2_GET_ERROR
+.globl ___SDL2_STATIC_BUILD
 .p2align 2
 _start:
 _START:
     B L_START
-
+    
 ; --- Veneer Section ---
     ; --- End Veneer Section ---
 
@@ -158,6 +159,7 @@ L_Point_CREATE_Entry_0:
     MOV X0, X9
     BL _WRITES
     LDR X9, [X27, #8] ; x
+    MOV X0, X9
     BL _WRITEN
     ADRP X9, L_str1@PAGE
     ADD X9, X9, L_str1@PAGEOFF
@@ -165,6 +167,7 @@ L_Point_CREATE_Entry_0:
     MOV X0, X9
     BL _WRITES
     LDR X9, [X27, #16] ; y
+    MOV X0, X9
     BL _WRITEN
     ADRP X9, L_str2@PAGE
     ADD X9, X9, L_str2@PAGEOFF
@@ -181,7 +184,7 @@ L_0:
     LDP x25, x26, [x29, #88]
     LDP x27, x28, [x29, #104]
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L_Point_getX:
@@ -191,6 +194,7 @@ L_Point_getX:
     MOV X15, X0    // Move parameter '_this' from X0 to X15
 L_Point_getX_Entry_0:
     LDR X9, [X15, #8] ; x
+    MOV X0, X9
     B L_1
     B L_Point_getX_Exit_1
 L_Point_getX_Exit_1:
@@ -198,7 +202,7 @@ L_Point_getX_Exit_1:
 L_1:
     LDP x19, x28, [x29, #24]
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L_Point_getY:
@@ -208,6 +212,7 @@ L_Point_getY:
     MOV X15, X0    // Move parameter '_this' from X0 to X15
 L_Point_getY_Entry_0:
     LDR X9, [X15, #16] ; y
+    MOV X0, X9
     B L_2
     B L_Point_getY_Exit_1
 L_Point_getY_Exit_1:
@@ -215,7 +220,7 @@ L_Point_getY_Exit_1:
 L_2:
     LDP x19, x28, [x29, #24]
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L_Point_set:
@@ -242,6 +247,7 @@ L_Point_set_Entry_0:
     MOV X0, X9
     BL _WRITES
     LDR X9, [X27, #8] ; x
+    MOV X0, X9
     BL _WRITEN
     ADRP X9, L_str1@PAGE
     ADD X9, X9, L_str1@PAGEOFF
@@ -249,6 +255,7 @@ L_Point_set_Entry_0:
     MOV X0, X9
     BL _WRITES
     LDR X9, [X27, #16] ; y
+    MOV X0, X9
     BL _WRITEN
     ADRP X9, L_str2@PAGE
     ADD X9, X9, L_str2@PAGEOFF
@@ -265,7 +272,7 @@ L_3:
     LDP x25, x26, [x29, #88]
     LDP x27, x28, [x29, #104]
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L_Point_RELEASE:
@@ -280,7 +287,7 @@ L_Point_RELEASE_Exit_1:
 L_4:
     LDP x19, x28, [x29, #24]
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L_START:
@@ -381,12 +388,12 @@ L_5:
     LDP x19, x27, [x29, #24]
     LDR X28, [X29, #40] ; Restored Reg: X28 @ FP+40
     MOV SP, X29 ; Deallocate frame by moving FP to SP
-    LDP x29, x30, [x31]
+    LDP x29, x30, [SP, #0]
     ADD SP, SP, #16 ; Deallocate space for saved FP/LR
     RET
 L___veneer_:
-    movz x16, #29136
-    movk x16, #631, lsl #16
+    movz x16, #44824
+    movk x16, #295, lsl #16
     movk x16, #1, lsl #32
     movk x16, #0, lsl #48
     blr x16
@@ -424,11 +431,10 @@ L_str1:
     .long 0x0
     .long 0x0
 L_str2:
-    .quad 0x3
+    .quad 0x2
     ; (upper half)
     .long 0x29
-    .long 0x2a
-    .long 0x4e
+    .long 0xa
     .long 0x0
     .long 0x0
 L_str3:
@@ -453,7 +459,7 @@ L_str3:
     .long 0x0
     .long 0x0
 L_str4:
-    .quad 0x13
+    .quad 0x12
     ; (upper half)
     .long 0x43
     .long 0x72
@@ -472,8 +478,7 @@ L_str4:
     .long 0x2e
     .long 0x2e
     .long 0x2e
-    .long 0x2a
-    .long 0x4e
+    .long 0xa
     .long 0x0
     .long 0x0
 L_str5:
@@ -509,16 +514,14 @@ L_str6:
     .long 0x0
     .long 0x0
 L_str7:
-    .quad 0x4
+    .quad 0x2
     ; (upper half)
-    .long 0x2a
-    .long 0x4e
-    .long 0x2a
-    .long 0x4e
+    .long 0xa
+    .long 0xa
     .long 0x0
     .long 0x0
 L_str8:
-    .quad 0x17
+    .quad 0x16
     ; (upper half)
     .long 0x53
     .long 0x65
@@ -541,8 +544,7 @@ L_str8:
     .long 0x2e
     .long 0x2e
     .long 0x2e
-    .long 0x2a
-    .long 0x4e
+    .long 0xa
     .long 0x0
     .long 0x0
 L_str9:
@@ -573,10 +575,9 @@ L_str9:
     .long 0x0
     .long 0x0
 L_str10:
-    .quad 0x2
+    .quad 0x1
     ; (upper half)
-    .long 0x2a
-    .long 0x4e
+    .long 0xa
     .long 0x0
     .long 0x0
 L_Point_vtable:
@@ -588,6 +589,9 @@ L_Point_vtable:
 
 .section __DATA,__data
 .p2align 3
+    .long 0x0
+    .long 0x0
+    .long 0x0
     .long 0x0
     .long 0x0
     .long 0x0
